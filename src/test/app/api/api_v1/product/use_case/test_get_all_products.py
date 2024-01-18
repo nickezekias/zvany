f089db718115eaa9b74f6cc17869cac4b1d035b5
Nick Ezekiel <nickezekias@gmail.com>
2024-01-18 23:51:04 +0000

    added tests for get_all_products use case

---
+++ src/test/app/api/api_v1/product/use_case/test_get_all_products.py
@@ -0,0 +1,20 @@
+from src.app.api.api_v1.product.adapter.response.product_response import ProductPostResponse
+from src.app.api.api_v1.product.use_case.get_all_products import GetAllProducts
+from src.domain.product.i_product_presenter import IProductPresenter
+from src.domain.product.i_product_repository import IProductRepository
+from src.test.app.api.api_v1.product.adapter.presenter.test_product_presenter import TestProductPresenter
+from src.test.app.api.api_v1.product.adapter.repository.test_product_mariadb_repository import TestProductMariaDbRepository
+
+
+class TestGetAllProducts:
+    repository: IProductRepository = TestProductMariaDbRepository()
+    presenter: IProductPresenter = TestProductPresenter()
+
+    async def test_execute(self, filter_query: str = ""):
+        #FIXME: define tests for when there is a filter_query
+        if filter_query:
+            pass
+        res = await GetAllProducts(self.repository, self.presenter).execute(filter_query)
+        if len(res) > 0:
+            assert isinstance(res[0], ProductPostResponse)
+        assert isinstance(res, list)
