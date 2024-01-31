b46ec3a14830185079fe0dd642109b171eab2d64
Nick Ezekiel <nickezekias@gmail.com>
2024-01-31 23:51:46 +0000

    remove unused id attr on data object

--- src/app/api/api_v1/product/adapter/request/product_metadata_request.py
+++ src/app/api/api_v1/product/adapter/request/product_metadata_request.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
 class ProductMetadataRequest(BaseModel):
-    id: str = ""
     key: str
     value: str
--- src/app/api/api_v1/product/adapter/response/product_metadata_response.py
+++ src/app/api/api_v1/product/adapter/response/product_metadata_response.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
 class ProductMetadataResponse(BaseModel):
-    id: str
     key: str
     value: str
