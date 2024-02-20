bf3269629ac4820d0c57e17b9d598b6c82172bae
Nick Ezekiel <nickezekias@gmail.com>
2024-02-20 10:10:36 +0000

    added output_error_object_not_found also corrected type issues and formatting

--- src/app/core/adapter/presenter.py
+++ src/app/core/adapter/presenter.py
@@ -1,24 +1,27 @@
-from abc import ABC
-from typing import TypeVar
 from fastapi import HTTPException, status
-import json
 
 from src.domain.base.i_presenter import IPresenter
-I = TypeVar('I')
-O = TypeVar('O')
+
 
 class Presenter(IPresenter):
-  def output(self, data: I) -> O:
-    return super().output(data)
+    def output(self, data: object) -> object:
+        pass
 
-  def output_error_server_db_commit(self, details: str | None = None) -> None:
-    raise HTTPException(
-      status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-      detail={ "message": "server.errors.db.couldNotCommitToDB", "details": details }
-    )
-  
-  def output_error_domain_validation(self, details: dict | str) -> None:
-    raise HTTPException(
-      status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-      detail=str(details)
-    )
+    def output_error_server_db_commit(self, details: str | None = None) -> None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "message": "server.errors.db.couldNotCommitToDB",
+                "details": details,
+            },
+        )
+
+    def output_error_domain_validation(self, details: dict | str) -> None:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(details)
+        )
+
+    def output_error_object_not_found(self, details: dict | str) -> None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail={str(details)}
+        )
--- src/domain/base/i_presenter.py
+++ src/domain/base/i_presenter.py
@@ -1,18 +1,19 @@
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
 
-I = TypeVar('I')
-O = TypeVar('O')
 
 class IPresenter(ABC):
     @abstractmethod
-    def output(self, data: I) -> O:
+    def output(self, data: object) -> object:
         pass
 
     @abstractmethod
-    def output_error_server_db_commit(self, details: str | None  = None) -> None:
+    def output_error_server_db_commit(self, details: str | None = None) -> None:
         pass
 
     @abstractmethod
     def output_error_domain_validation(self, details: dict | str) -> None:
+        pass
+
+    @abstractmethod
+    def output_error_object_not_found(self, details: dict | str) -> None:
         pass
